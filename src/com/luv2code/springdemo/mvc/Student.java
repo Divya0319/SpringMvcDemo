1d95b11e46b1f2efa4a1e513321ba8c720b565d8
Divya Gupta <divygupta0319@gmail.com>
2023-02-19 03:58:46 +0530

    Populating dropdown list from a properties file done

--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -1,26 +1,13 @@
 package com.luv2code.springdemo.mvc;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-
 public class Student {
 	
 	private String firstName;
 	private String lastName;
 	private String country;
 	
-	private ArrayList<String> countryOptions;
 	
 	public Student() {
-		
-		countryOptions = new ArrayList<>();
-		
-		countryOptions.add("Brazil");
-		countryOptions.add("Germany");
-		countryOptions.add("France");
-		countryOptions.add("India");
-		countryOptions.add("United States of America");
-		countryOptions.add("Japan");
 
 		
 	}
@@ -51,11 +38,6 @@ public class Student {
 
 	public void setCountry(String country) {
 		this.country = country;
-	}
-
-
-	public ArrayList<String> getCountryOptions() {
-		return countryOptions;
 	}
 	
 
