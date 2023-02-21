9c9356505aca59d3d684c9fc1c2023a17ba6fd88
Divya Gupta <divygupta0319@gmail.com>
2023-02-21 17:55:47 +0530

    Adding custom validation rule by creating a custom annotation, and a validator class holding business logic for validation

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
 public class Customer {
 	
 	private String firstName;
@@ -18,6 +20,9 @@ public class Customer {
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to 10")
 	private Integer freePasses;
+	
+	@CourseCode(value="TOP", message="must start with TOP")
+	private String courseCode;
 	
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars or digits")
@@ -54,5 +59,14 @@ public class Customer {
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	
 
 }
